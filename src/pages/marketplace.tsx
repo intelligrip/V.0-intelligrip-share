8bc020c1cce1493095a94d0d92a3c5cac726c905
Murtaza Ahmad <murtaza.ahmad0075@gmail.com>
2025-02-06 23:22:13 +0500

    feat: Add TyreMarketplace

--- src/pages/marketplace.tsx
+++ src/pages/marketplace.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head"
 import { DashboardLayout } from "@/components/dashboard/DashboardLayout"
 import { BicycleMarketplace } from "@/components/marketplace/BicycleMarketplace"
+import { TyreMarketplace } from "@/components/marketplace/TyreMarketplace"
 
 export default function MarketplacePage() {
   return (
@@ -10,21 +11,21 @@ export default function MarketplacePage() {
         <meta name="description" content="Buy and sell bicycles in our marketplace" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
-       <main className="flex min-h-screen">
-                   {/* Sidebar - Dashboard */}
-                   <div className="w-64">
-                     <DashboardLayout />
-                   </div>
-           
-                   {/* Main Content */}
-                   <div className="flex-1 p-4 md:p-8">
-                    <BicycleMarketplace/>
-                    <BicycleMarketplace/>
-                    <BicycleMarketplace/>
-                   </div>
-        </main>
-      
+
+      <main className="flex min-h-screen">
+        {/* Sidebar - Dashboard */}
+        <div className="w-64">
+          <DashboardLayout />
+        </div>
+
+        {/* Main Content */}
+        <div className="flex-1 p-4 md:p-8">
+          <BicycleMarketplace />
+          <TyreMarketplace />
+          {/* <BicycleMarketplace/> */}
+        </div>
+      </main>
+
     </>
   )
 }
